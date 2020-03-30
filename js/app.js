a44d529050254fa4eed84396e7c85e5bff358b72
Evgenij Tugaj <79255220650@ya.ru>
2020-03-30 21:19:18 +0300

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let money = 90000,
+    income = 'фриланс',
+    addExpenses = 'интернет, ТАКСИ, коммуналка',
+    deposit = false,
+    mission = 1000000,
+    period = 12,
+    budgetDay = money / 30;
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`, `Цель заработать ${mission} рублей`);
+console.log('addExpenses: ', addExpenses.toLowerCase().split(' '));
+console.log('budgetDay: ', budgetDay);
